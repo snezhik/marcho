aabc1cbbd194e084759e1a34154dc1d98b8c0281
snzhik <geek_snezhana@mail.ru>
2021-04-07 18:08:54 +0300

    add marcho

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,41 @@
 $(function(){
 
+  $('.blog-page__slder').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/angle-left-solid.svg" alt="arrow-right"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/angle-right-solid.svg" alt="arrow-right"></button>',
+    infinite:false,
+  });
+
+
+  $('.product-tabs__top-item').on('click', function(e){
+
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+   
+
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect:true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical:true,
+    draggable:false
+
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable:false,
+    arrows:false,
+    fade:true,
+
+  });
+
   $('.shop-content__filter-btn').on('click', function(){
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active')
@@ -14,7 +50,7 @@ $(function(){
   });
 
 
- $('.select-style') .styler();
+ $('.select-style, .product-one__item-num') .styler();
 
 $('.filter-price__input').ionRangeSlider({
   type: "double",
@@ -45,7 +81,7 @@ $(".star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
-    readOnly: true
+    readOnly: true,
   });
 
 
@@ -91,5 +127,8 @@ $(".star").rateYo({
   
   const deadline = '2021-07-12';
   initializeClock('promo__clock', deadline);
+
+
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ const concat        = require('gulp-concat');
 const autoprefixer  = require('gulp-autoprefixer');
 const uglify        = require('gulp-uglify');
 const del = require('del');
-const imagemin    = require('gulp-imagemin');
+const imagemin      = require('gulp-imagemin');
+const rename        = require('gulp-rename');
+const nunjucksRender= require('gulp-nunjucks-render');
 const browserSync   = require('browser-sync').create();
 //const mainMin = require('./app/js/main.min');
 
@@ -18,10 +20,21 @@ function browsersync() {
     })
 }
 
+
+function nunjucks(){
+    return src('app/*.njk')
+    .pipe(nunjucksRender())
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
+
 function styles() {
-    return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'expanded'}))
-    .pipe(concat('style.min.css'))
+    return src('app/scss/*.scss')
+    .pipe(scss({outputStyle: 'compressed'}))
+    // .pipe(concat())
+    .pipe(rename({
+        suffix : '.min'
+    }))
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 10 versions'], 
         grid: true
@@ -78,6 +91,7 @@ function cleanDist(){
 
 function watching(){
     watch(['app/scss/**/*.scss'], styles);
+    watch(['app/*.njk'], nunjucks);
     watch(['app/js/**/*.js','!app/js/main.min.js'],scripts);
     watch(['app/**/*.html']).on('change', browserSync.reload);
 }
@@ -87,10 +101,11 @@ exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.nunjucks = nunjucks;
 exports.build = build;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(nunjucks, styles, scripts, browsersync, watching);
 
